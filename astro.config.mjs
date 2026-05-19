f17750ef625b7746b5a2f3f269f0633d43387079
Cashews939 <skm79925@googlemail.com>
2026-05-19 23:17:06 +0200

    Starter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import keystatic from '@keystatic/astro';
+import tailwindv4 from '@tailwindcss/vite'; // Das neue Plugin laden
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), keystatic()],
+  output: 'server',
+  vite: {
+    plugins: [tailwindv4()], // Tailwind v4 direkt in den Compiler schieben
+  },
+});
